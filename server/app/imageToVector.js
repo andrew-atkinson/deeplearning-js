3cc7cfb8aa5a9ff59e63963ed2ae049a268b9552
andrew-atkinson <blind.sam@gmail.com>
2017-09-29 19:09:44 -0400

    adds: 3D matrix to 1D vector

---
+++ server/app/imageToVector.js
@@ -0,0 +1,10 @@
+const {reshape, size} = require('mathjs')
+
+/**
+ * transforms a 3d matrix into a 1 dimensional vector for processing
+ * @param {matrix} image takes a 3D matrix
+ * @returns a 1d vector
+ */
+module.exports = function (image, arse, bottom) {
+  return reshape(image, [image.size()[0] * image.size()[1] * image.size()[2]])
+}
