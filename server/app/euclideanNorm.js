af64347371e3bd883e857a214ebaaea711173ce0
andrew-atkinson <blind.sam@gmail.com>
2017-10-01 17:43:27 -0400

    fixes: syntax error

--- server/app/euclideanNorm.js
+++ server/app/euclideanNorm.js
@@ -2,7 +2,7 @@ const math = require('mathjs')
 
 module.exports = function (row) {
   let total = 0;
-  if (row.length >= 1) 
+  if (row.size()[0] >= 1) 
     row.forEach(value => total += value * value)
   else 
     total = row
