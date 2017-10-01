23c2492a06b51548b9790262807142099fbb371e
andrew-atkinson <blind.sam@gmail.com>
2017-10-01 18:44:48 -0400

    chore: removed one unneeded func call

--- server/app/normalizeRows.js
+++ server/app/normalizeRows.js
@@ -8,9 +8,9 @@ module.exports = function (matrix) {
   let rows = numRows(matrix),
     columns = numColumns(matrix)
   for (var i = 0; i < rows; i++) {
-    let rownorm = euclidNorm(matrix.subset(index([i], range(0, matrix.size()[1]))))
+    let rowNorm = euclidNorm(matrix.subset(index([i], range(0, columns))))
     for (var j = 0; j < columns; j++) {
-      let normalisedValue = matrix.subset(math.index(i, j)) / rownorm
+      let normalisedValue = matrix.subset(math.index(i, j)) / rowNorm
       matrix.subset(math.index(i, j), normalisedValue)
     }
   }
