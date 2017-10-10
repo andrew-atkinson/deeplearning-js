598d5d35f3c9aa2ab5be68457be5ddac860d872e
andrew-atkinson <blind.sam@gmail.com>
2017-10-09 21:54:43 -0400

    chore: dumb syntax mistakes

--- server/app/utils/lr_utils.js
+++ server/app/utils/lr_utils.js
@@ -5,18 +5,18 @@ const numColumns = require('./numColumns')
 const {Access} = require('hdf5/lib/globals')
 
 export default () => {
-    train_dataset = new hdf5.File('datasets/train_catvnoncat.h5', Access.ACC_RDONLY)
-    train_set_x_orig = matrix(train_dataset["train_set_x"].slice()) 
-    train_set_y_orig = matrix(train_dataset["train_set_y"].slice()) 
+    const train_dataset = new hdf5.File('datasets/train_catvnoncat.h5', Access.ACC_RDONLY)
+    let train_set_x_orig = matrix(train_dataset["train_set_x"].slice()) 
+    let train_set_y_orig = matrix(train_dataset["train_set_y"].slice()) 
 
-    test_dataset = new hdf5.File('datasets/test_catvnoncat.h5', Access.ACC_RDONLY)
-    test_set_x_orig = matrix(test_dataset["test_set_x"].slice()) 
-    test_set_y_orig = matrix(test_dataset["test_set_y"].slice())
+    const test_dataset = new hdf5.File('datasets/test_catvnoncat.h5', Access.ACC_RDONLY)
+    let test_set_x_orig = matrix(test_dataset["test_set_x"].slice()) 
+    let test_set_y_orig = matrix(test_dataset["test_set_y"].slice())
 
-    classes = matrix(test_dataset["list_classes"].slice())
+    const classes = matrix(test_dataset["list_classes"].slice())
     
-    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
     
     return [train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes]
   }
