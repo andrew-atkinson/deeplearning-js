04aa447e8771c1c2cc6b06fb91d5f9c801371bac
andrew-atkinson <blind.sam@gmail.com>
2017-10-21 21:49:06 -0400

    refactor: generalises logistic func, adds func parameter to allow other activation funcs

--- server/app/logisticFunc.js
+++ server/app/logisticFunc.js
@@ -1,5 +1,9 @@
 const {dot, transpose, subset, index} = require('mathjs')
 const sigmoid = require('./sigmoid')
+const tanh = require('./tanh')
+const reLU = require('./reLU')
+const leakyReLU = require('./leakyReLU')
+
 
 /**
  * performs for the logistic function
@@ -7,6 +11,7 @@ const sigmoid = require('./sigmoid')
  * @param {Matrix} w Weights
  * @param {Matrix} X An array of examples
  * @param {Number} b bias scalar
+ * @param {function} func generalises the logistic function, adding a default parameter of sigmoid (can allow e.g. tanh, reLU, etc)
  * @returns {Number}
  */
-module.exports = (w, X, b) => sigmoid(dot(transpose(w).subset(index(i)), X.subset(index(i))) + b)
+module.exports = (w, X, b, func = sigmoid) => func(dot(transpose(w).subset(index(i)), X.subset(index(i))) + b)
